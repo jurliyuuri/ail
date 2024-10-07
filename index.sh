4a58a0932d418dbef28b418da9e4e2746903e41c
meloviliju <ritterderschwerter178@gmail.com>
2024-10-07 12:48:21 +0900

    index.sh

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# DOWNLOAD
+# 1. DOWNLOAD
 wget -O dict.txt 'https://docs.google.com/spreadsheets/d/1Han8vd6RHVaT0CV6svMh840AgeAiqi7ieY2kbuBbfEk/export?gid=0&format=tsv'
 
 # 2. PREPROCESS
